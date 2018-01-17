764e9742b0ebbc36f6a13bd29c2e53ea25b80da6
Jessica Huang <jhuang@wireless-10-146-80-92.public.utexas.edu>
2018-01-17 16:18:08 -0600

    print file names from box

--- manage.py
+++ manage.py
@@ -41,29 +41,35 @@ def get_box():
     client = Client(oauth)
     root_folder = client.folder(folder_id='0')
     shared_folder = root_folder.create_subfolder('shared_folder')
+    items = client.folder(folder_id='w0l7qg69b3tiseyslfaij9bcx6g1l0co').get_items(limit=100, offset=0)
+
+    for item in items:
+        print item.get()['name']
+
     # uploaded_file = shared_folder.upload('/path/to/file')
     # shared_link = shared_folder.get_shared_link()
 
-@manager.command
-def print_files():
-    """Shows basic usage of the Google Drive API.
 
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
-
-    results = service.files().list(
-        pageSize=10,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+# @manager.command
+# def print_files():
+#     """Shows basic usage of the Google Drive API.
+#
+#     Creates a Google Drive API service object and outputs the names and IDs
+#     for up to 10 files.
+#     """
+#     credentials = get_credentials()
+#     http = credentials.authorize(httplib2.Http())
+#     service = discovery.build('drive', 'v3', http=http)
+#
+#     results = service.files().list(
+#         pageSize=10,fields="nextPageToken, files(id, name)").execute()
+#     items = results.get('files', [])
+#     if not items:
+#         print('No files found.')
+#     else:
+#         print('Files:')
+#         for item in items:
+#             print('{0} ({1})'.format(item['name'], item['id']))
 
 @manager.command
 def dropdb():
