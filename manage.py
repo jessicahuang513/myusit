5b2159be78cf7f145c876fea7bf707ec4f6955bc
technology@usiteam.org <archiejain1021@gmail.com>
2018-04-12 15:40:59 -0500

    import func

--- manage.py
+++ manage.py
@@ -11,6 +11,7 @@ from boxsdk import OAuth2
 from boxsdk import Client
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, Float, insert
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import func
 # from votingchallenge import votesession, get_users_from_vote
 # from signin import signsession
 
